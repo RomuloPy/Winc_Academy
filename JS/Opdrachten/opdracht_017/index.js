d444bf9cb950a4fc8357af10963a48c6bfeecf54
Romulo Santos <romulo.santos@hotmail.com>
2021-12-15 22:14:36 +0100

    Update index.js

--- JS/Opdrachten/opdracht_017/index.js
+++ JS/Opdrachten/opdracht_017/index.js
@@ -4,10 +4,13 @@ let firstName = 'Rómulo'
 let lastName = 'Santos'
 console.log(firstName, lastName);
 
+// "if" is a reserved keyword, so i can't create a variable with "if"
+
 let numbers = 4+4;
 let strings = "4"+"4";
 console.log(numbers)
-console.log(strings)
+console.log(strings)    // it isn't possible because the result will not appear as a adding but a joining of two separated strings.
+                        // On this case instead of 8, we'll get 44.
 
 let adding = 4+4;
 console.log(adding);
