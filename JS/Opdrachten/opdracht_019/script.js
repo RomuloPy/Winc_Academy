c7f43b3617560e0b13faaad7031fb52f7b2cc98b
Romulo Santos <romulo.santos@hotmail.com>
2021-12-19 17:44:50 +0100

    opdracht_019

---
+++ JS/Opdrachten/opdracht_019/script.js
@@ -0,0 +1,14 @@
+console.log("Hello Winc Academy students");
+
+// This is a large sum
+/* console.log(1230941 * 1823794);
+
+console.log(637263 / 54); */
+
+// 4.   No, I don't see the outcome of my comment in the console because a comment
+//      is just a note to the code and will not be readed by the editor as code.
+ 
+// 6. No, i can't see the result of the sum in the console because i just commented it.
+// The editor doesn't read it as code.
+
+
